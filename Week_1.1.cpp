8ef41b6f3af1d81b0aa8941e99120bf706af8e8d
Yash Maheshwari <42332890+YashMaheshwari1267@users.noreply.github.com>
2019-01-31 12:56:50 +0530

    Add files via upload

---
+++ Week_1.1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout<<"Enter a number to check it as Even or Odd:";
+	cin>>n;
+	(n%2==0?cout<<n<<" is Even.":cout<<n<<" is Odd");
+}
